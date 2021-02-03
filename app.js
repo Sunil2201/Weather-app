3d6d83f53ee12048431f73f2e49d27f2610a54ca
Sunil Ballani <72138289+Sunil2201@users.noreply.github.com>
2021-02-03 23:19:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function showError(error){
 }
 //Get Weather from api
 function getWeather(latitude,longitude){
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`
     console.log(api);
 
     fetch(api)
